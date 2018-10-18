6437e9305d23d685769d7f1c57e304d775f78522
huimintoh <hmintoh@gmail.com>
2018-10-18 15:22:00 +0800

    to establish db connection

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require("./app");
+//mongodb
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/express-books-api", { useNewUrlParser: true });
 
+const db = mongoose.connection;
+db.once("open", open => {
+  console.log("connected to db")
+})
+db.on("error", error => {
+  console.error("unable to connect to db")
+})
+
+//connection to localhost
+const app = require("./app");
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Listening on port ${server.address().port}...`);
 });
