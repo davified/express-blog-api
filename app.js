2ebb99be9ff0bb7289f9c9d3f60405f7a6a8fb95
huimintoh <hmintoh@gmail.com>
2018-10-18 15:42:16 +0800

    created Book and Author models

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+const mongoose = require("mongoose")
 const express = require("express");
 const logger = require("morgan");
 
-const index = require("./routes/index");
-const books = require("./routes/books");
-
+//body parser
 const app = express();
-
 app.use(logger("dev"));
 app.use(express.json());
 
+//models
+const Book = require("./models/book")
+const Author = require("./models/author")
+
+//routes
+const index = require("./routes/index");
+const books = require("./routes/books");
 app.use("/", index);
 app.use("/books", books);
 
