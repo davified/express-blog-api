a573f6321163e1adfc24951e963266ce903a312f
Brian <tjtbrian@gmail.com>
2018-10-19 10:04:37 +0800

    Added Author route

--- models/book.js
+++ models/book.js
@@ -1,16 +1,17 @@
-const mongoose = require('mongoose')
-const {Schema} = mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const bookSchema = Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: String,
-        required: true
-    }
-})
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: "Author",
+    required: true
+  }
+});
 
-const Book = mongoose.model("Book", bookSchema)
-module.exports = Book
+const Book = mongoose.model("Book", bookSchema);
+module.exports = Book;
